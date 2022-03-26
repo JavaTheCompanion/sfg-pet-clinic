1c253010049edaeed99db7708dfaba1d578be63a
Senthilkumar Sellamuthu <senthil.sellamuthu@gmail.com>
2022-03-26 18:42:53 +0530

    Adding base entity. Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,14 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class PetType {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PetType extends BaseEntity {
 
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
