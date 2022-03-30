628d837e721090e218932f6eab8b77c5e91bf194
Senthilkumar Sellamuthu <senthil.sellamuthu@gmail.com>
2022-03-30 22:23:34 +0530

    Added Vet Speciality and associated it with Vet

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+}
